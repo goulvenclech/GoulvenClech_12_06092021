d683ef93834858ba9739c2eef3da5b111e7a5bfe
Goulven Nev <nevilain@gmail.com>
2021-09-29 19:24:56 +0200

    add RadialBarChart

---
+++ src/Components/D3/RadialBarChart.js
@@ -0,0 +1,79 @@
+import * as d3 from 'd3';
+/**
+ * A simple radial bar chart
+ * @param {float} score - % of personal goal remaining 
+ */
+export function radialBarChart(score) {
+    // Change the size of the graph
+    const radius = 110
+    const barWidth = 20
+    // Create the chart
+    var chart = d3.select("#chart")
+        .attr("width", radius * 3)
+        .attr("height", radius * 3)
+    ;
+    // Clean obsolete chart 🗑️
+    chart.selectChildren().remove()
+   // Background circle
+    chart.append("svg:circle")
+        .attr('cx', radius * 1.5)
+        .attr('cy', radius * 1.5)
+        .attr('r', radius + barWidth / 2)
+        .attr('fill', 'white');
+    ;
+    // Register our arc
+    var arc = d3.arc()
+        .innerRadius(radius)
+        .outerRadius(radius + barWidth)
+        .cornerRadius(barWidth / 2)
+        .startAngle(0)
+    ;
+    // Create a path, and make it an arc with its style & data
+	chart.append("svg:path")
+        // this is mutable
+        .datum({
+            endAngle: -0.1,
+        })
+        .attr("class", "chart1")
+        .style("fill", "red")
+        .attr("transform", "translate(" + radius * 1.5 + "," + radius * 1.5 + ")")
+        // use our arc
+        .attr("d", arc)
+        // launch a transition arcTween with the new endAngle 
+        .transition()
+            .duration(750)
+            .call(arcTween, score * Math.PI * -2);
+    ;
+    // Register our fantastic transition ✨
+    function arcTween(transition, newFinishAngle) {
+        transition.attrTween("d", function (d) {
+            var interpolateEnd = d3.interpolate(d.endAngle, newFinishAngle);
+            return function (t) {
+            	d.endAngle = interpolateEnd(t);
+                return arc(d);
+            };
+        });
+    }
+    // For the end, let's put some text ✍🏼
+    chart.append("svg:text")
+        .attr("x", radius * 1.5)
+        .attr("y", radius * 1.4)
+        .style("font-size", "2rem")
+        .style("text-anchor", "middle")
+        .text(score * 100 + "%")
+    ;
+    chart.append("svg:text")
+        .attr("x", radius * 1.5)
+        .attr("y", radius * 1.4 + 25)
+        .style("font-size", "1.2rem")
+        .style("text-anchor", "middle")
+        .text("de votre")
+    ;
+    chart.append("svg:text")
+        .attr("x", radius * 1.5)
+        .attr("y", radius * 1.4 + 50)
+        .style("font-size", "1.2rem")
+        .style("text-anchor", "middle")
+        .text("objectif")
+    ;
+}
--- src/Components/User/GraphScore.res
+++ src/Components/User/GraphScore.res
@@ -1,10 +1,11 @@
-@module("../D3/Graph") external test: (~score: float) => unit = "test"
+@module("../D3/RadialBarChart") external radialBarChart: (~score: float) => unit = "radialBarChart"
 
 @react.component
 let make = (~id: string) => {
 
   let userData = ApiHooks.useUserData(id)
-  test(~score = userData.todayScore)
+  // will fill our SVG with some d3 black magic 🧙🏼‍♂️🪄⚗️✨
+  radialBarChart(~score = userData.todayScore)
 
   <section className="bg-gray-100 rounded-lg py-4 px-6 w-96">
     <h3 className="font-semibold text-lg"> 
